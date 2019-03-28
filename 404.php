9e36eddb460996119ae1095b887a31878df21cc5
Hendy Dwi Putranto <hendy.putranto@kompas.com>
2019-03-28 15:35:05 +0700

    update

--- 404.php
+++ 404.php
@@ -14,7 +14,7 @@ get_header();
 		<main id="main" class="site-main w-4/5 my-16 mx-auto justify-center flex">
 
 			<div id="notfound">
-				<div class="notfound flex justify-center flex-col">
+				<div class="notfound flex justify-center flex-col mb-2">
 					<div class="notfound-404 justify-center flex flex-col">
 						<div class="justify-center flex">
 							<h3>Oops! Page not found</h3>
@@ -23,8 +23,18 @@ get_header();
 							<h1><span>4</span><span>0</span><span>4</span></h1>
 						</div>
 					</div>
-					<h2>we are sorry, but the page you requested was not found. Comeback here if your loss "<a href="<?php echo home_url(); ?>">here</a>"</h2>
+					<div class="flex flex-col w-full">
+						<div class="justify-center flex">
+							<h2>we are sorry, but the page you requested was not found. Comeback here if your loss "<a href="<?php echo home_url(); ?>">here</a>"</h2>
+						</div>
+						<?php if ( is_active_sidebar('sidebar-1')){ ?>
+						<div class="justify-center flex">
+							<h2>Or you can use this widget for help.</h2>
+						</div>
+						<?php } ?>
+					</div>
 				</div>
+				<?php get_sidebar(); ?>
 			</div>
 
 		</main><!-- #main -->
